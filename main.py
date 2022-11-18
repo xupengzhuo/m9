7097ed749839a81ebc28a6a940e3878e5e6438b9
xupengzhuo <xupengzhuo@gmail.com>
2022-11-18 18:18:09 +0800

    update

--- main.py
+++ main.py
@@ -118,13 +118,13 @@ class m9util:
 
         return os.path.join(rtpth, tn)
 
-    def find_project(p):
+    def find_project(p, trypath=True):
         try:
-            if os.path.exists(os.path.join(p, ".m9/meta.json")):
-                return os.path.abspath(p)
             if p in os.listdir(ABSPATH_PROJECT):
                 with open(os.path.join(ABSPATH_PROJECT, p)) as jfp:
                     return json.load(jfp)["project_dir"]
+            if trypath and os.path.exists(os.path.join(p, ".m9/meta.json")):
+                return os.path.abspath(p)
 
             return False
         except Exception as error:  # FIXME: add
@@ -292,14 +292,12 @@ class m9:
 
     def deploy(project, runtime, pack_relpath, args):
         print(project, runtime, pack_relpath, args)
-
         env = os.environ.copy()
 
-        if not m9util.find_project(project):
+        if not (target_dir := m9util.find_project(project, trypath=False)):  # create project object here
+
             proj_abspath = os.path.abspath(os.path.join(CURRENT_PATH, pack_relpath))
             plink = os.path.join(ABSPATH_PROJECT, project)
-            print(plink, 33333)
-            print(proj_abspath, "1111111")
 
             with open(f"{pack_relpath}/.m9/meta.json", "r+") as jfp:
                 r = json.load(jfp)
@@ -308,12 +306,17 @@ class m9:
                 jfp.truncate()
                 jfp.write(json.dumps(r))
             os.symlink(f"{proj_abspath}/.m9/meta.json", plink)
+            target_dir = proj_abspath
+            
+        else:
+            env["M9_ARGS_targetdir"] = target_dir  # path to deply code
 
-        if not m9util.find_runtime(f"{project}.{runtime}"):
-            env["M9_ARGS_init_deploy"] = "true"
+        if not m9util.find_runtime(f"{project}.{runtime}"): 
             with open(os.path.join(ABSPATH_RUNTIME, f"{project}.{runtime}.json"), "w") as jfp:
-                json.dump({"created_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "project_dir": proj_abspath, "project": project}, jfp)
+                json.dump({"created_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "project_dir": target_dir, "project": project}, jfp)
 
+        env['M9_PROJECT'] = project
+        env['M9_RUNTIME'] = runtime
         subprocess.run(args=args, cwd=pack_relpath, env=env)
 
 
